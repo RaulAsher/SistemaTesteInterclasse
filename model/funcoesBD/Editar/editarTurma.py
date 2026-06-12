3e76a1db967a46dbcf138741972a33cdcbbcacda
RaulAsher <Raulasher51@gmail.com>
2026-06-11 22:39:30 -0300

    Feature 5.7.2: Correção de editarTurma

--- model/funcoesBD/Editar/editarTurma.py
+++ model/funcoesBD/Editar/editarTurma.py
@@ -3,9 +3,8 @@ from ..Cadastrar.criarConexao import criarConexao, database
 def editarTurma(turma_antiga, turma_nova, icone_url):
     conn = criarConexao()
     cursor = conn.cursor()
-    cursor.execute(
-        "UPDATE turmas SET pk_nome_turma = %s, icone_url = %s WHERE pk_nome_turma = %s",
-        (turma_nova, icone_url, turma_antiga)
-    )
+    
+    cursor.execute("UPDATE turmas SET pk_nome_turma = %s, icone_url = %s WHERE pk_nome_turma = %s",(turma_nova, icone_url, turma_antiga))
+    
     conn.commit()
     conn.close()
